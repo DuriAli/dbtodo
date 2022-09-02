107957b35f39fb4a1864a7ca01a867cf27f36226
duriali <abduri123@gmail.com>
2022-09-02 11:23:49 +0200

    fixed bug

--- lib/services/fade_route_transition.dart
+++ lib/services/fade_route_transition.dart
@@ -1,15 +1,13 @@
 import 'package:flutter/material.dart';
+
 class FadeRoute<T> extends MaterialPageRoute<T> {
-  FadeRoute({ WidgetBuilder builder, RouteSettings settings })
+  FadeRoute({WidgetBuilder builder, RouteSettings settings})
       : super(builder: builder, settings: settings);
 
   @override
-  Widget buildTransitions(BuildContext context,
-      Animation<double> animation,
-      Animation<double> secondaryAnimation,
-      Widget child) {
-    if (settings.isInitialRoute)
-      return child;
+  Widget buildTransitions(BuildContext context, Animation<double> animation,
+      Animation<double> secondaryAnimation, Widget child) {
+    if (settings.name == '/') return child;
     // Fades between routes. (If you don't want any animation,
     // just return child.)
     return new FadeTransition(opacity: animation, child: child);
